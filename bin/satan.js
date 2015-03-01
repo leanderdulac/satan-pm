b8291e52dbd4d95c3b706420110d62cd4eda324d
Jonathan Lima <greenboxal@gmail.com>
2015-03-01 12:14:54 -0300

    Add first version.

---
+++ bin/satan.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var _ = require('lodash');
+var program = require('commander');
+var Satan = require('../lib');
+var pkg = require('../package');
+
+program.version(pkg.version);
+program.usage('[options] -- [args...]');
+program.option('-s, --script [path]', 'worker script path');
+program.option('-d, --working-directory [path]', 'working directory');
+program.option('-i, --instances [count]', 'number of workers to spawn');
+program.option('-n, --node-arguments [args]', 'custom arguments to nodejs');
+program.option('-p, --scheduling-policy', 'cluster scheduling policy(none or rr)');
+program.option('-c, --config [path]', 'config file path');
+program.parse(process.argv);
+
+var config = {};
+
+if (program.config) {
+	config = JSON.parse(fs.readFileSync(program.config));
+}
+
+new Satan({
+	script: program.script,
+	arguments: program.args,
+	nodeArguments: program.nodeArguments,
+	workingDirectory: program.workingDirectory,
+	instances: program.instances,
+	schedulingPolicy: program.schedulingPolicy
+}, program.config).start();
+
