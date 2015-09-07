c831ebdd7de39f836f3a9dc4b7295a29b7fe6d2b
Jonathan Lima <greenboxal@gmail.com>
2015-09-06 22:11:36 -0300

    Add refactored version.

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var requiem = require('cluster-requiem');
-requiem.initialize();
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
 }).listen(1339, '127.0.0.1');
-requiem.trackSocket(server);
+process.on('exit', function() {
+	console.log('Tchau!');
+});
 console.log('Server running at http://127.0.0.1:1337/');
+
