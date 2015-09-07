c494e07e2a61b203a7f65e863d240b2d974e1b6a
Jonathan Lima <greenboxal@gmail.com>
2015-09-06 22:15:46 -0300

    Add worker id to process title.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 }).listen(1339, '127.0.0.1');
 process.on('exit', function() {
-	console.log('Tchau!');
+	console.log('Tchau! ' + require('cluster').worker.id);
 });
 console.log('Server running at http://127.0.0.1:1337/');
 
