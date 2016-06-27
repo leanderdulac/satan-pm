e32b7a0953566da772f94c876f0bc03dc88d0d36
Jonathan Lima <greenboxal@gmail.com>
2016-06-27 18:35:00 -0300

    Fix pid file option

--- bin/satan.js
+++ bin/satan.js
@@ -13,7 +13,7 @@ program.option('-i, --instances [count]', 'number of workers to spawn');
 program.option('-n, --node-arguments [args]', 'custom arguments to nodejs');
 program.option('-p, --scheduling-policy', 'cluster scheduling policy(none or rr)');
 program.option('-c, --config [path]', 'config file path');
-program.option('-p, --pid-file [path]', 'pid file path');
+program.option('-P, --pid-file [path]', 'pid file path');
 program.parse(process.argv);
 
 new Satan({
